b5f59e23895488c8d112083ffff33d611c19cb63
Can Berk Durmus <canberkdurmus@gmail.com>
2021-04-15 00:59:15 +0300

    Print 10-mer and mutants, increase check period

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from motif_search import MotifSearch
 
 if __name__ == '__main__':
     file_name = 'dna'
-    dna = Dna(file_name, generate=False).lines
+    dna = Dna(file_name, generate=False)
+    dna_lines = dna.lines
+    print('10-Mer: ', dna.ten_mer)
+    print('Mutated 10-Mers: ', dna.mutated_list)
 
     for k in range(9, 12):
-        MotifSearch('random', dna, k, check_period=500).print_results()
-        MotifSearch('gibbs', dna, k, check_period=500).print_results()
+        MotifSearch('random', dna_lines, k, check_period=500).print_results()
+        MotifSearch('gibbs', dna_lines, k, check_period=500).print_results()
